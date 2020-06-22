6c543b2e92541669a0d0ac87e56a3c30c6d106ea
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-06-22 11:55:21 +0200

    chapter 13 HW - done

--- operating-systems-three-easy-pieces/chapter-13-homework/memory-user.c
+++ operating-systems-three-easy-pieces/chapter-13-homework/memory-user.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	printf("%i", argc);
+	int bytes = atoi(argv[1]);
+	printf("%s", "working...\n");
+	printf("%ld\n", (long)getpid());
+	
+	
+	bool *ptr;
+	ptr = (bool*) malloc(bytes);
+
+	while(true) {
+		for (int i = 0; i < bytes; i++) {
+			ptr[i] = !ptr[i];
+		}
+	}
 }
