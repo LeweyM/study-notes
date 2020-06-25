298a6dcb73687cb6c2d6f554d1642d51a894d31e
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-06-25 15:42:30 +0200

    mygrep implementation

--- operating-systems-three-easy-pieces/projects/intro-projects/mygrep.c
+++ operating-systems-three-easy-pieces/projects/intro-projects/mygrep.c
@@ -6,26 +6,21 @@
 // hello
 
 int matches_from_here(char* line, char* search_term, int search_term_length, int i, int depth) {
-	printf("pop");
 	if (depth == search_term_length) {
-		printf("\nboop:%c\nbrap:%c\n", *(line+i), *(search_term+i));
 		return 1;
 	}
-	if (*(line + i) == *(search_term + i)){
+	if (*(line + i) == *(search_term + depth)){
 		return matches_from_here(line, search_term, search_term_length, i+1, depth+1);
 	}
 	return 0;
 }
 
 int matches(char* line, char* search_term, int line_len) {
-	int search_term_len = strlen(search_term)-1;
-	for (int i = 0; i < line_len; i++) {
-		if (i + search_term_len >= line_len) {
-			return 0;
-		} else {
-			int c = matches_from_here(line, search_term, search_term_len, i, 0);
-			if (c == 1) return 1;
-		}
+	
+	int search_term_len = strlen(search_term);
+	for (int i = 0; i < line_len - search_term_len; i++) {
+		int c = matches_from_here(line, search_term, search_term_len, i, 0);
+		if (c == 1) return 1;
 	}
 	return 0;
 }
@@ -44,7 +39,9 @@ int main(int argc, char *argv[]) {
 	ssize_t linelen;
 	while((linelen = getline(&line, &linecap, fp)) > 0) {
 		int contains = matches(line, search_term, linelen);
-		printf("line: %sresult: %i\n", line, contains);
+		if (contains == 1) {
+			printf("%s", line);
+		}
 	}
 	fclose(fp);
 }
